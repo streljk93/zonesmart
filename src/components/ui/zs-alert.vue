2a430524d8841dc3f4469709325e413155744b25
streljk93 <strel.jk.93@gmail.com>
2021-01-19 10:17:43 +0200

    SP-21 Create zs-alert component

---
+++ src/components/ui/zs-alert.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+    .zs-alert(:class="`zs-alert_${type}`")
+        .zs-alert--icon
+            span
+                i.icon-check-copy-1
+        .zs-alert--message
+            slot
+</template>
+
+<script>
+export default {
+    name: 'ZsAlert',
+    props: {
+        type: {
+            type: String,
+            default: 'info',
+        },
+    },
+}
+</script>
+
+<style lang="sass" scoped>
+.zs-alert
+    border-radius: $--border-md
+    display: inline-flex
+    height: 45px
+    padding: 0 20px
+    align-items: center
+    background: $--color-background
+    &_success
+        background: $--color-accent
+    &--icon span
+        border-radius: 50%
+        height: 26px
+        width: 26px
+        background: $--color-white
+        display: inline-flex
+        align-items: center
+        justify-content: center
+        margin-right: 15px
+        i
+            color: $--color-typo
+            font-weight: bold
+            font-size: 16px
+    &--message
+        @include mix--typography-body1
+        flex: 1
+</style>
