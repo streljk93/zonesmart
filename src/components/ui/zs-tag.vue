f4078fa526e9757dfd77b5f9ac4793da846c4f68
streljk93 <strel.jk.93@gmail.com>
2021-01-19 17:29:49 +0200

    SP-8 Add icon type in zs-tag component

--- src/components/ui/zs-tag.vue
+++ src/components/ui/zs-tag.vue
@@ -1,11 +1,23 @@
 <template lang="pug">
-    .zs-tag
-        slot
+    .zs-tag(:class="`zs-tag_${type}`")
+        span(v-if="type === 'icon'")
+            i(:class="$slots.default[0].text")
+        span(v-else)
+            slot
 </template>
 
 <script>
 export default {
     name: 'ZsTag',
+    props: {
+        type: {
+            type: String,
+            default: 'text',
+        },
+    },
+    created() {
+        console.log(this.$slots.default[0].text)
+    }
 }
 </script>
 
@@ -14,9 +26,20 @@ export default {
     @include mix--typography-caption
     border-radius: $--border-sm
     display: inline-flex
+    justify-content: center
     align-items: center
-    height: 22px
-    padding: 0 8px
     background: $--color-accent
     color: $--color-white
+
+    &_text
+        height: 22px
+        padding: 0 8px
+    &_icon
+        border-radius: 50%
+        height: 20px
+        width: 20px
+        padding: 0
+        i
+            font-size: 14px
+            font-weight: 900
 </style>
--- src/stories/zs-tag.stories.js
+++ src/stories/zs-tag.stories.js
@@ -5,11 +5,22 @@ export default {
     component: ZsTag,
 }
 
-const Template = () => ({
+const Template = (args, {argTypes}) => ({
+    props: Object.keys(argTypes),
     components: {ZsTag},
     template: `
-        <zs-tag>ebay</zs-tag>
+        <zs-tag :type="type">{{value}}</zs-tag>
     `,
 })
 
 export const Default = Template.bind({})
+Default.args = {
+    type: 'text',
+    value: 'ebay',
+}
+
+export const Icon = Template.bind({})
+Icon.args = {
+    type: 'icon',
+    value: 'icon-check-copy-1',
+}
