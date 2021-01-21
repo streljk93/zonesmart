e6b3c8ead972f745413404623545c0522769cd7a
streljk93 <strel.jk.93@gmail.com>
2021-01-21 14:31:23 +0200

    Add default background for zs-avatar component

--- src/components/ui/zs-avatar.vue
+++ src/components/ui/zs-avatar.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-    img.zs-avatar(:src="src" :style="`width: ${size}px; height: ${size}px;`")
+    img.zs-avatar(
+        :src="src"
+        :style="`width: ${size}px; height: ${size}px;`"
+        :class="{'zs-avatar--background': !src}"
+    )
 </template>
 
 <script>
@@ -24,4 +28,6 @@ export default {
     overflow: hidden
     display: inline-block
     object-fit: cover
+    &--background
+        background: $--color-typo-label
 </style>
