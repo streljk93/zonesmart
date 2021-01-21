4742c494042319ff324072b6f554eb64d5020a56
streljk93 <strel.jk.93@gmail.com>
2021-01-21 14:34:43 +0200

    SP-18 Add refresh which expired token

--- src/config/setting/axios.js
+++ src/config/setting/axios.js
@@ -3,6 +3,23 @@ import axios from 'axios'
 
 // app
 import config from '@/config'
+import {handlerHttpError} from '@/utils/transport'
 
-axios.defaults.baseURL = config.api;
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL = config.api
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.interceptors.request.use((config) => {
+    const auth = JSON.parse(localStorage.getItem('auth'))
+    const access = auth.access
+
+    if (access) config.headers.authorization = `JWT ${access}`
+
+    return config
+})
+axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        handlerHttpError(error)
+
+        return Promise.reject(error)
+    },
+)
