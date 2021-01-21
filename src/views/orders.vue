e3f3d030bd275805ed65f9ded33040ef48110a44
streljk93 <strel.jk.93@gmail.com>
2021-01-21 06:52:19 +0200

    SP-27 Make orders page

--- src/views/orders.vue
+++ src/views/orders.vue
@@ -1,10 +1,24 @@
 <template lang="pug">
-    p.orders orders
+    zs-master
+        template(#default)
+            zs-header
+                template(#title) Таблица заказов
+                template(#actions)
+                    zs-button(theme="primary" size="sm") Отправления
+                    zs-button(size="sm") Экспортировать
+            zs-orders-table-container
 </template>
 
 <script>
+// Components
+import ZsMaster from '@/components/layout/zs-master'
+import ZsHeader from '@/components/ui/zs-header'
+import ZsButton from '@/components/ui/zs-button'
+import ZsOrdersTableContainer from '@/components/zs-orders-table-container'
+
 export default {
     name: 'Orders',
+    components: {ZsMaster, ZsHeader, ZsButton, ZsOrdersTableContainer},
 }
 </script>
 
