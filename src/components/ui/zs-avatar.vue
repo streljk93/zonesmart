2af21e7ad5bf16ca58c86f091ebcc1047855051e
streljk93 <strel.jk.93@gmail.com>
2021-01-21 06:34:13 +0200

    SP-31 Create zs-avatar component

---
+++ src/components/ui/zs-avatar.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+    img.zs-avatar(:src="src" :style="`width: ${size}px; height: ${size}px;`")
+</template>
+
+<script>
+export default {
+    name: 'ZsAvatar',
+    props: {
+        src: {
+            type: String,
+            default: '',
+        },
+        size: {
+            type: Number,
+            default: 40,
+        }
+    },
+}
+</script>
+
+<style lang="sass" scoped>
+.zs-avatar
+    border-radius: $--border-radius-md
+    overflow: hidden
+    display: inline-block
+    object-fit: cover
+</style>
